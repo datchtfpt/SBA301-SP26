553c48a1bdc0e060fe060981729b1798265c64b5
Le Thanh Dat <lethanhdat20072005@gmail.com>
2026-01-08 10:46:30 +0700

    Add Lab1 to Lab

---
+++ Lab/Lab1/src/components/Header.jsx
@@ -0,0 +1,23 @@
+import { Button } from "react-bootstrap";
+import { Container, Nav, Navbar } from "react-bootstrap";
+
+function Header() {
+    return (
+        <>
+            <header>
+                <Navbar bg="primary" data-bs-theme="dark">
+                    <Container>
+                        <Navbar.Brand href="#home">HDPE STORE</Navbar.Brand>
+                        <Nav className="me-auto">
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#features">Features</Nav.Link>
+                            <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        </Nav>
+                    </Container>
+                </Navbar>
+                <h1>Welcome to Le Thanh Dat Orchid Website</h1>
+            </header>
+        </>
+    );
+}
+export default Header;
---
+++ Lab/Lab1/src/data/listOfOrchid.js
@@ -0,0 +1,65 @@
+export const OrchidsData =
+    [
+        {
+            "id": "1",
+            "orchidName": "Ceasar 4N",
+            "description": "The most beautiful orchid in the word",
+            "category": "Dendrobium",
+            "isSpecial": true,
+            "image": "images/orchid1.png"
+        },
+        {
+            "id": "2",
+            "orchidName": "Mini Yaya",
+            "description": "The top 2 beautiful orchids in the word",
+            "category": "Dendrobium",
+            "isSpecial": true,
+            "image": "images/orchid2.png"
+        },
+        {
+            "id": "3",
+            "orchidName": "Crystallinum",
+            "description": "The top 3 beautiful orchids in the word",
+            "category": "Dendrobium",
+            "isSpecial": true,
+            "image": "images/orchid3.png"
+        },
+        {
+            "id": "4",
+            "orchidName": "Damari",
+            "description": "The top 4 beautiful orchids in the word",
+            "category": "Dendrobium",
+            "isSpecial": false,
+            "image": "images/orchid4.png"
+        },
+        {
+            "id": "5",
+            "orchidName": "Busuangense",
+            "description": "The top 5 beautiful orchids in the word",
+            "isSpecial": true,
+            "image": "images/orchid5.png"
+        },
+        {
+            "id": "6",
+            "orchidName": "Pink Moth",
+            "description": "The top 6 beautiful orchids in the word",
+            "image": "images/orchid6.png",
+            "category": "Oncidium"
+        },
+        {
+            id: "7",
+            "orchidName": "Alba",
+            "description": "The top 7 beautiful orchids in the word",
+            "category": "Dendrobium",
+            "isSpecial": true,
+            "image": "images/orchid7.png"
+        },
+        {
+            "id": "8",
+            "orchidName": "Singapore yellow dendro",
+            "description": "The top 8 beautiful orchids in the word",
+            "category": "Dendrobium",
+            "isSpecial": true,
+            "image": "images/orchid8.png"
+        }
+    ]
