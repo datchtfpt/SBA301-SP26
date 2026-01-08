c3ffb592c02de5bd9229e17fa78bfc3303efc353
Le Thanh Dat <lethanhdat20072005@gmail.com>
2026-01-08 08:44:19 +0700

    Add Slot3

---
+++ Slot3/ex1/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Footer from './components/Footer.jsx'
+import Header from './components/Header.jsx'
+import Orchid from './components/Orchid.jsx'
+
+
+
+function App() {
+
+    return (
+        <>
+            <Header />
+
+            <Orchid />
+            <Footer avatar="/images/image.png" name="Le Thanh Dat" contact="lethanhdat20072005@gmail.com"/>
+        </>
+    )
+}
+
+export default App
